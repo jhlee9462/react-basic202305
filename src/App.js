8f1c4a52308398a297918a4e23e85f9d70c647d5
jhlee9462 <jhlee9462@gmail.com>
2023-05-22 11:47:03 +0900

    101일차 끝

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import SayHello from "./SayHello";
+import SubmitEvent from "./component/chap01_event/SubmitEvent";
+import ClickEvent from "./component/chap01_event/ClickEvent";
+import ChangeEvent from "./component/chap01_event/ChangeEvent";
 
 function App() {
+
+    const looping = () => {
+        const helloList = [];
+
+        for (let i = 0; i < 5; i++) {
+            helloList.push(<SayHello />);
+        }
+
+        return helloList;
+    };
+
+    // jsx 문법에서는 스크립트 코드를 직접 사용 불가능
+    // {} 안에서 함수호출문이나 변수참조를 할 수 있음.
   return (
-      <h1>리액트 초보입니다~~~ ㅋ</h1>
+      <>
+         <SubmitEvent />
+      </>
   );
+
 }
 
 export default App;
